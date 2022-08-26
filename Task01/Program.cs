52a18b3125e08965deca7ce685fe6ff73d969902
NKompa <nkompa7@gmail.com>
2022-08-26 19:00:42 +0300

    Find second number

---
+++ Task01/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 456 -> 5. 782 -> 8
+
+Console.WriteLine("Введите трёхзначное число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = num1/10;
+int result = num2%10;
+
+Console.WriteLine(result);
