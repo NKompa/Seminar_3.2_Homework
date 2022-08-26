cd6bc171c556469de1b0b3461ee5f192f8b32f52
NKompa <nkompa7@gmail.com>
2022-08-27 01:42:02 +0300

    Weekday is day off

---
+++ Task03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. 6 -> да, 1 -> нет.
+
+Console.WriteLine("Введите порядковый номер дня недели: ");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+
+if (weekDay > 7 || weekDay == 0)
+{
+    Console.WriteLine("Введён неверный номер");
+}
+else if (weekDay ==6 || weekDay == 7)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
